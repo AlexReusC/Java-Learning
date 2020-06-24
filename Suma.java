6aee836175004ada8dd44c620e8931ac731a0c41
AlexReusC <55596302+AlexReusC@users.noreply.github.com>
2020-06-23 20:41:14 -0500

    Programa que suma dos enteros

---
+++ Suma.java
@@ -0,0 +1,10 @@
+public class Suma{
+  public static void main(String args[]){
+    int numero_1 = 5;
+    int numero_2 = 2;
+    int resultado = 0;
+
+    resultado = numero_1 + numero_2;
+    System.out.println("El valor de tu suma: " + resultado);
+  }
+}
