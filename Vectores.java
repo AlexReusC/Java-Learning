1274506e12e0dc9e753eda506939d3ce6b7fe180
AlexReusC <55596302+AlexReusC@users.noreply.github.com>
2020-06-25 21:36:37 -0500

    Uso de vectores unidimensional

---
+++ Vectores.java
@@ -0,0 +1,17 @@
+public class Vectores{
+  public static void main(String main[]){
+
+    int numeros[] = new int[5];
+
+    numeros[0] = 5;
+    numeros[1] = 220;
+    numeros[2] = 8;
+    numeros[3] = 458;
+    numeros[4] = 22;
+
+    for(int i=0; i<5; i++){
+    System.out.print("[" + numeros[i] + "]");
+  }
+
+  }
+}
